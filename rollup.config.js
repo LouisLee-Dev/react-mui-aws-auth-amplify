4cf1ca566d17faeb8e80b3e3c83aa573d8a9cc6b
LouisLee-Dev <louislee0996@gmail.com>
2024-05-08 10:13:53 -0400

    feat:last update

---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+import del from "rollup-plugin-delete";
+import babel from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import url from "@rollup/plugin-url";
+import json from "@rollup/plugin-json";
+import svgr from "@svgr/rollup";
+import analyze from "rollup-plugin-analyzer";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+import scss from "rollup-plugin-scss";
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
+
+const input = "src/index.tsx";
+const globals = {
+  react: "React",
+  "react-dom": "ReactDOM",
+};
+
+const plugins = [
+  del({ targets: "dist/*", runOnce: true }),
+  typescript({ useTsconfigDeclarationDir: true }),
+  json(),
+  external(),
+  resolve({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
+  replace({ __VERSION__: `'${pkg.version}'`, preventAssignment: true }),
+  analyze({ summaryOnly: true }),
+  commonjs(),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "./node_modules/**",
+    include: ["src/**"],
+  }),
+  scss({
+    insert: true,
+    processor: (css) =>
+      postcss([autoprefixer])
+        .process(css)
+        .then((result) => result.css),
+  }),
+  url(),
+  svgr({ icon: true }),
+];
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        globals,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+        globals,
+      },
+    ],
+    plugins: plugins,
+  },
+];
---
+++ src/Authenticator.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable no-case-declarations */
+import React, { useEffect } from "react";
+import { Auth, Hub } from "aws-amplify";
+import { useAuthContext } from "./AuthProvider";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PageSignIn from "./pages/SignIn";
+import PageSignUp from "./pages/SignUp";
+import PageForgotPassword from "./pages/ForgotPassword";
+import PageVerifyCode from "./pages/VerifyAccount";
+import PageChangePassword from "./pages/ChangePassword";
+
+type UserInfo = {
+  id: string;
+  name: string;
+  role: string;
+};
+
+type AuthenticatorProps = {
+  children: React.ReactElement;
+  services: {
+    onSignIn?: (user: UserInfo) => void;
+    onSignOut?: () => void;
+  };
+  authUserTokenKey: string;
+  roles: {
+    type: string;
+    value: string;
+  }[];
+};
+
+function Authenticator({ children, services: { onSignIn, onSignOut }, authUserTokenKey, roles }: AuthenticatorProps) {
+  const [{ authenticated, isAuthenticating }, { setAuthenticated, setIsAuthenticating }] = useAuthContext();
+
+  const listener = async (data: any) => {
+    const { data: userData } = data.payload;
+
+    let session, token;
+    switch (data.payload.event) {
+      case "signIn":
+        session = await Auth.currentSession();
+        token = session.getAccessToken().getJwtToken();
+        localStorage.setItem(authUserTokenKey, token);
+        onSignIn &&
+          onSignIn({
+            id: userData.username,
+            name: userData.attributes.email,
+            role: roles[0].value || "user",
+          });
+        setAuthenticated(true);
+        setIsAuthenticating(false);
+        break;
+      case "signUp":
+        break;
+      case "signOut":
+        setAuthenticated(false);
+        setIsAuthenticating(false);
+        onSignOut && onSignOut();
+        break;
+      case "signIn_failure":
+        setAuthenticated(false);
+        setIsAuthenticating(false);
+        break;
+      case "tokenRefresh":
+        session = await Auth.currentSession();
+        token = session.getAccessToken().getJwtToken();
+        localStorage.setItem(authUserTokenKey, token);
+        break;
+      case "tokenRefresh_failure":
+        break;
+      case "configured":
+        break;
+      default:
+    }
+  };
+
+  const startUpdateAccessTokenUsingRefreshToken = () => {
+    const timer = setInterval(async () => {
+      const cognitoUser = await Auth.currentAuthenticatedUser();
+      const { refreshToken } = cognitoUser.getSignInUserSession();
+      cognitoUser.refreshSession(refreshToken, (err: any, session: { accessToken: { jwtToken: string } }) => {
+        if (err) {
+          clearTimeout(timer);
+          Auth.signOut();
+        } else {
+          localStorage.setItem(authUserTokenKey, session?.accessToken?.jwtToken);
+        }
+      });
+    }, 5 * 1000 * 60);
+  };
+
+  useEffect(() => {
+    Hub.listen("auth", listener);
+    startUpdateAccessTokenUsingRefreshToken();
+    async function getUserInfo() {
+      const user = await Auth.currentUserInfo();
+      try {
+        const cognitoUser = await Auth.currentAuthenticatedUser();
+        const { refreshToken } = cognitoUser.getSignInUserSession();
+        cognitoUser.refreshSession(refreshToken, (err: any, session: { accessToken: { jwtToken: string } }) => {
+          if (err) {
+            Auth.signOut();
+          } else {
+            localStorage.setItem(authUserTokenKey, session?.accessToken?.jwtToken);
+            setAuthenticated(true);
+            setIsAuthenticating(false);
+            onSignIn &&
+              onSignIn({
+                id: user.username,
+                name: user.attributes.email,
+                role: roles[0].value || "user",
+              });
+          }
+        });
+      } catch {
+        Auth.signOut();
+      }
+    }
+    getUserInfo();
+  }, []); //eslint-disable-line
+
+  if (isAuthenticating) {
+    return <div></div>;
+  }
+
+  if (!authenticated) {
+    return (
+      <Routes>
+        <Route path="/" index element={<PageSignIn></PageSignIn>}></Route>
+        <Route path="/signup" element={<PageSignUp></PageSignUp>}></Route>
+        <Route path="/forgotpassword" element={<PageForgotPassword></PageForgotPassword>}></Route>
+        <Route path="/verify" element={<PageVerifyCode></PageVerifyCode>}></Route>
+        <Route path="/changepassword" element={<PageChangePassword></PageChangePassword>}></Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  }
+
+  return children;
+}
+
+export default Authenticator;
